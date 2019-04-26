e7b5ab2a209d5e7504697b1f30bcaf9b99149adc
Michael Dauner <mdauner@gmail.com>
2019-04-26 15:15:53 -0500

    Add widget variants

--- server/resolvers/widget.js
+++ server/resolvers/widget.js
@@ -1,8 +1,29 @@
 const widgetResolvers = {
     Query: {
         widgets: () => [
-            { name: 'Premium Widget' },
-            { name: 'Ultimate Widget' },
+            {
+                name: 'Premium Widget',
+
+                variants: [
+                    {
+                        color: 'GREEN',
+                        size: 'SMALL',
+                        numAvailable: 3,
+                        price: 12.5,
+                    },
+                ],
+            },
+            {
+                name: 'Ultimate Widget',
+                variants: [
+                    {
+                        color: 'GREEN',
+                        size: 'MEDIUM',
+                        numAvailable: 3,
+                        price: 12.5,
+                    },
+                ],
+            },
         ],
     },
 }
--- server/typeDefs/types/widgetType.js
+++ server/typeDefs/types/widgetType.js
@@ -1,8 +1,28 @@
 const { gql } = require('apollo-server-express')
 
 const widgetType = gql`
+    enum Color {
+        GREEN
+        RED
+        BLUE
+    }
+
+    enum Size {
+        SMALL
+        MEDIUM
+        LARGE
+    }
+
+    type WidgetVariant {
+        color: Color!
+        size: Size!
+        numAvailable: Int!
+        price: Float!
+    }
+
     type Widget {
         name: String!
+        variants: [WidgetVariant!]!
     }
 `
 

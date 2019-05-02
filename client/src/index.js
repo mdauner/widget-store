73f3cc3ec2f56e3ea92b63ab5227a28b2b561405
Michael Dauner <mdauner@gmail.com>
2019-05-01 21:46:39 -0500

    Add router

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo-hooks'
 import ApolloClient from 'apollo-boost'
+import { Router } from '@reach/router'
+import { Page as WidgetPage } from './components/Widget'
+import { Page as OrderPage } from './components/Order'
 import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import '@reach/dialog/styles.css'
 
 const client = new ApolloClient({
-    uri: '/graphql',
+  uri: '/graphql'
 })
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Router>
+      <WidgetPage path="/" />
+      <OrderPage path="/orders/:orderId" />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
